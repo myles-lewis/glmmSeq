501250b42b0091f25b16718e966776d9cb75aed7
hhy082 <hhy082@qmul.ac.uk>
2021-02-16 10:37:01 +0000

    Update with code review changes

--- R/modelPlot.R
+++ R/modelPlot.R
@@ -1,32 +1,72 @@
 #' Model plot
 #'
 #' Model plots to show the overall differences between groups and over time
-#' @param glmmResult A glmmSeq object
+#' @param glmmResult A glmmSeq object created by 
+#' \code{\link[glmmSeq:glmmSeq]{glmmSeq::glmmSeq()}}.. 
 #' @param geneName Gene name to plot
 #' @param x1Label The name of the first (inner) x parameter
 #' @param x2Label The name of the second (outer) x parameter
 #' @param xTitle Title for the x axis
+#' @param yTitle Title for the y axis
+#' @param title Plot title. If NULL gene name is used. 
 #' @param logTransform Whether to perform a log10 transform on the y axis
 #' @param shapes The marker shape (default=21)
+#' @param x2Offset Vertical adjustment to secondary x-axis (default=3)
 #' @param colours The marker colour, default=c('red', 'blue')
+#' @param overlap Logical whether x2Label fits should be plotted overlapping one
+#' another.
+#' @param addErrorbars Logical whether to add error bars. 
 #' @param graphics Which graphic system to use (options = "base" or "ggplot")
-#' @return Returns a model plot
+#' @param ... Other parameters to pass to 
+#' \code{\link[graphics:plot]{graphics::plot()}} or 
+#' \code{\link[ggplot2:theme]{ggplot2::theme()}}. 
+#' @return Returns a plot with the glmm fit for a given gene
 #' @importFrom ggplot2 ggplot geom_boxplot geom_point geom_line theme_classic
 #' scale_fill_manual scale_shape_manual labs geom_text scale_x_discrete
 #' coord_cartesian aes scale_color_manual theme scale_y_continuous
-#' scale_x_continuous aes_string margin
-#' @importFrom graphics arrows axis lines mtext plot
+#' scale_x_continuous aes_string margin geom_errorbar element_text
+#' @importFrom graphics arrows axis lines mtext plot legend
 #' @export
+#' @examples
+#' data(PEAC_minimal_load)
+#' 
+#' disp <- apply(tpm, 1, function(x){ 
+#' (var(x, na.rm=TRUE)-mean(x, na.rm=TRUE))/(mean(x, na.rm=TRUE)**2) 
+#' })
+#' 
+#' MS4A1Fit <- glmmSeq(~ Timepoint * EULAR_6m + (1 | PATID),
+#'                      id = 'PATID',
+#'                      countdata = tpm['MS4A1', ],
+#'                      metadata = metadata,
+#'                      dispersion = disp['MS4A1'],
+#'                      verbose=FALSE)
+#'                      
+#' modelPlot(MS4A1Fit,
+#'           "MS4A1", 
+#'           x1Label="Timepoint", 
+#'           x2Label="EULAR_6m", 
+#'           colours = c('skyblue', 'goldenrod1', 'mediumvioletred'),
+#'           xTitle="Time")
+
 
 modelPlot <- function(glmmResult,
                       geneName,
                       x1Label="Timepoint",
-                      x2Label, 
+                      x2Label,
                       xTitle=NULL,
-                      logTransform=FALSE, 
+                      yTitle="Gene Expression",
+                      title=NULL,
+                      logTransform=FALSE,
                       shapes=21,
-                      colours=c('red', 'blue'),
-                      graphics="ggplot") {
+                      colours=c('blue'),
+                      x2Offset=6, 
+                      lineWidth=1, 
+                      markerSize=5,
+                      fontSize=10,
+                      overlap=TRUE,
+                      addErrorbars=TRUE, 
+                      graphics="ggplot", 
+                      ...) {
   
   if(! graphics %in% c("ggplot", "base")){
     stop("graphics must be either 'ggplot' or 'base'")
@@ -34,14 +74,21 @@ modelPlot <- function(glmmResult,
   if(! geneName %in% rownames(glmmResult@predict)){
     stop("geneName must be in rownames(glmmResult@predict)")
   }
+  if(ncol(glmmResult@modelData) != 2){
+    stop(paste("These plots only work for interactions between two variable.", 
+               "Therefore nrow(glmmResult@modelData) should be 2."))
+  }
+  
   
   # Set up the plotting data
   modelData <- glmmResult@modelData
-  s <- nrow(modelData)
-  modelData$y <- glmmResult@predict[geneName, 1:s]
-  modelData$LCI <- glmmResult@predict[geneName, 1:s + s]
-  modelData$UCI <- glmmResult@predict[geneName, 1:s + s*2]
-  labs <- levels(modelData[, x2Label])
+  outLabels <- apply(glmmResult@modelData, 1, 
+                     function(x) paste(x, collapse="_"))
+  modelData$y <- glmmResult@predict[geneName, paste0("y_",  outLabels)]
+  modelData$LCI <- glmmResult@predict[geneName, paste0("LCI_",  outLabels)]
+  modelData$UCI <- glmmResult@predict[geneName, paste0("UCI_",  outLabels)]
+  x2Values <- levels(modelData[, x2Label])
+  x1Values <- levels(factor(modelData[, x1Label]))
   modelData[, x2Label] <- as.numeric(modelData[, x2Label])
   
   # Define the plotting parameters
@@ -51,63 +98,119 @@ modelPlot <- function(glmmResult,
   if (length(colours)==1) colours <- rep(colours, maxX2)
   yCover <- glmmResult@predict[geneName, ]
   yLim <- range(yCover[is.finite(yCover) & yCover != 0])
-  x <- as.numeric(modelData[,x1Label]) + 
-    (as.numeric(modelData[,x2Label])-1) * maxX2
+  x <- as.numeric(modelData[,x1Label]) +
+    (as.numeric(modelData[,x2Label])-1) * maxX1
   p <- glmmResult@stats[geneName, grepl("P_", colnames(glmmResult@stats))]
   pval <- vapply(p, format, FUN.VALUE="1", digits=2)
+  modelData$group <- factor(modelData[, x2Label], 
+                            labels=levels(factor(
+                              glmmResult@metadata[, x2Label])))
+  modelData$x1Numeric <- as.numeric(modelData[, x1Label])
+  modelData$x <- x
+  
+  if(is.null(title)) title <- geneName
+  
+  if(addErrorbars){
+    minLabel <- min(c(modelData$LCI, modelData$y), na.rm=TRUE)
+  } else { minLabel <- min(modelData$y, na.rm=TRUE) } 
   
   # Plot base graphics
   if(graphics == "base"){
     if(logTransform) log <- "y" else log <- ""
+    if(overlap) xUse <- as.numeric(factor(modelData[, x1Label])) else xUse <- x
     
-    plot(x, modelData$y, type='p', bty='l', las=1, xaxt='n',
-         cex.axis=1.5, cex.lab=1.8,
-         pch=shapes[modelData[,2]], bg=colours[modelData[,2]], cex=1.8, 
+    plot(xUse, modelData$y, type='p', bty='l', las=1, xaxt='n',
+         cex.axis=fontSize, cex.lab=fontSize,
+         cex=markerSize,
+         pch=shapes[modelData[,x2Label]], 
+         bg=colours[modelData[,x2Label]], 
          xlab=xTitle,
-         ylim=yLim, ylab=geneName, log=log,
+         ylim=yLim, ylab=yTitle, log=log,
+         ...,
          panel.first={
            for (i in 1:maxX2) {
-             lines(x[modelData[,2]==i], modelData$y[modelData[,2]==i],
+             lines(xUse[modelData[,2]==i], 
+                   modelData$y[modelData[,2]==i],
                    col=colours[i], lwd=2)
            }
-           arrows(x0=x, y0=modelData$LCI, y1=modelData$UCI,
-                  angle=90, code=3, length=0.08)
+           if(addErrorbars){
+             for (i in 1:maxX2) {
+               arrows(x0=xUse[modelData[,x2Label]==i],
+                      y0=modelData$LCI[modelData[,x2Label]==i],
+                      y1=modelData$UCI[modelData[,x2Label]==i],
+                      angle=90, code=3, length=0.08, col=colours[i])
+             }}
          })
-    axis(1, 1:(2*maxX2), labels=rep(unique(modelData[, x1Label]), maxX2), 
-         cex.axis=1.6)
-    if (length(labs)==3) labs <- gsub('\\..*', '', labs)
-    axis(1, 1:maxX2*2-0.5, labels=labs, line=1.5, cex.axis=1.4, tick=FALSE)
+    
+    if (length(x2Values)==3) x2Values <- gsub('\\..*', '', x2Values)
+    if(! overlap){
+      axis(1, length(x1Values)*(seq_along(x2Values)-1) +
+             length(x1Values)/2+0.5, 
+           labels=levels(factor(glmmResult@metadata[, x2Label])), 
+           line=1.5, cex.axis=fontSize, tick=FALSE)
+      axis(1, modelData$x, labels=modelData[, x1Label], cex.axis=fontSize)
+    } else{
+      axis(1, unique(xUse), labels=levels(factor(modelData[, x1Label])), 
+           cex.axis=fontSize)
+      legend("top", legend=levels(modelData$group),
+             col=colours, lty=1, cex=fontSize, box.lwd=0)
+    }
+    if(title!="") mtext(title, side=3, adj=0, padj=-3, cex=fontSize)
     
     mtext(bquote(
       paste("P"[.(x1Label)]*"=", .(pval[1]),
             ", P"[.(x2Label)]*"=", .(pval[2]),
             ", P"[paste(.(x1Label), ":", .(x2Label))]*"=",
-            .(pval[3]))), 
-      side=3, adj=0.04)
+            .(pval[3]))),
+      side=3, adj=0.04, cex=fontSize)
     
     # Plot ggplot
   } else{
-    df <- data.frame(x=x, y=modelData$y, c=factor(modelData[, 2]))
-    p <- ggplot(df, aes_string(x="x", y="y", fill="c", shape="c"), 
-                colour="black") +
-      geom_line(color="black") +
-      geom_point(size=3) +
-      scale_fill_manual(values=colours) +
-      scale_shape_manual(values=shapes) +
-      scale_x_continuous(labels=
-                           setNames(modelData[, x1Label], 
-                                    seq_along(modelData[, 1])), name="") +
+    
+    if(overlap){
+      
+      p <- ggplot(modelData, aes_string(x="x1Numeric", y="y", fill="group", 
+                                        shape="group", group="group", 
+                                        color="group")) +
+        geom_line(size=lineWidth) +
+        geom_point(size=markerSize) +
+        scale_x_continuous(labels=levels(modelData[, x1Label]), 
+                           breaks=unique(modelData$x1Numeric)) +
+        theme_classic() +
+        theme(plot.margin=margin(7,0,14,0), text=element_text(size=fontSize), 
+              ...) 
+    } else{
+      p <- ggplot(modelData, aes_string(x="x", y="y", fill="group", 
+                                        shape="group", color="group")) +
+        geom_line(size=lineWidth) +
+        geom_point(size=markerSize) +
+        scale_x_continuous(labels=modelData[, x1Label], 
+                           breaks=modelData$x, name="") +
+        geom_text(data=data.frame(
+          label=levels(factor(glmmResult@metadata[, x2Label])),
+          x=length(x1Values)*(seq_along(x2Values)-1) +
+            length(x1Values)/2+0.5,
+          y = minLabel), size=rel(3),
+          mapping=aes_string(label="label", x="x", y="y"), hjust = 0.5,
+          vjust=x2Offset, inherit.aes=FALSE)  +
+        
+        theme_classic() +
+        theme(legend.position = "none", plot.margin=margin(7,0,14,0), 
+              text=element_text(size=fontSize), ...)  +
+        coord_cartesian(clip = 'off', expand=TRUE)
+    }
+    p <- p +
+      scale_fill_manual(values=colours, name=x2Label) +
+      scale_color_manual(values=colours, name=x2Label) +
+      scale_shape_manual(values=shapes, name=x2Label) +
       labs(subtitle= bquote(
         paste("P"[.(x1Label)]*"=", .(pval[1]),
               ", P"[.(x2Label)]*"=", .(pval[2]),
               ", P"[paste(.(x1Label), ":", .(x2Label))]*"=",
-              .(pval[3]))), y=geneName, x=xTitle) +
-      theme_classic() +
-      theme(legend.position = "none", plot.margin=margin(0,0,14,0)) +
-      coord_cartesian(clip = 'off', expand=TRUE) +
-      annotate("text", label = levels(factor(glmmResult@metadata[, x2Label])),
-               x = c(1.5, 3.5), y = min(df$y), hjust = 0.5, vjust=5)  
+              .(pval[3]))), y=yTitle, x=xTitle, title=title) 
     
+    if(addErrorbars) p <- p + geom_errorbar(aes_string(ymin="LCI", ymax="UCI"), 
+                                            width=.25, size=lineWidth) 
     if(logTransform) p <- p + scale_y_continuous(trans='log10')
     
     return(p)
--- package_creator.R
+++ package_creator.R
@@ -1,6 +1,8 @@
 # Script to create the R package
-# path_package = "/media/gcpeac/Katriona/seq_mixed_models/glmmSeq" # workstation
-path_package = "~/Documents/Analyses/PhD_Projects/glmmSeq/" # laptop
+ # Set wd to pacakge directory
+
+# restart session
+.rs.restartR()
 
 library(devtools)
 library(roxygen2)
@@ -8,21 +10,18 @@ library(roxygen2)
 ## On inital set up
 
 # remove any lingering objects from the environment
-rm(fcPlot, aPlot, glmmQvals, glmmResults, glmmSeq, GlmmSeq, character_or_list, 
-   df_or_matrix, maPlotly, modelPlot, labs, glmmGene, geneName, glmmResult, maPlot)
-rm(list = c("predict"))
-rm(list = c(".__C__GlmmSeq", "pairedPlot"))
+rm(list=ls(all=T))
 
 # Set up documentation
-devtools::document(path_package)
-devtools::document(path_package)
+devtools::document()
+devtools::document()
 
 # Build the package
-devtools::build(path_package, vignettes=FALSE)
+devtools::build(vignettes=FALSE)
 
 # Install
-devtools::install(path_package)
-devtools::build_vignettes(path_package)
+devtools::install()
+devtools::build_vignettes()
 
 library(glmmSeq)
 
@@ -32,18 +31,17 @@ library(glmmSeq)
 ?modelPlot
 ?glmmGene
 
-
-
 # Test how "good" the package is
 tests = goodpractice::all_checks()[! grepl("covr", goodpractice::all_checks())]
 goodpractice::gp(checks=tests)
+
+
 devtools::spell_check()
-#spelling::update_wordlist(pkg = path_package)
+spelling::update_wordlist()
 
 
 
 # pkgdown
-usethis::use_pkgdown()
 pkgdown::build_site()
 
 
