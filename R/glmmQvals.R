a1cbea8a3caf7d040233ea9e81f611695d6351bb
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-08-04 20:15:20 +0100

    Update glmmQvals.R

--- R/glmmQvals.R
+++ R/glmmQvals.R
@@ -1,7 +1,7 @@
 #' Glmm Sequencing qvalues
 #'
 #' Add qvalue columns to the glmmSeq dataframe
-#' @param glmmResult A glmmSeq object created by
+#' @param object A glmmSeq/lmmSeq object created by
 #' \code{\link[glmmSeq:glmmSeq]{glmmSeq::glmmSeq()}}.
 #' @param cutoff Prints a table showing the number of probes considered
 #' significant by the pvalue cut-off (default=0.05)
@@ -20,33 +20,40 @@
 #' (var(x, na.rm=TRUE)-mean(x, na.rm = TRUE))/(mean(x, na.rm = TRUE)**2)
 #' })
 #' MS4A1glmm <- glmmSeq(~ Timepoint * EULAR_6m + (1 | PATID),
-#'                      id = "PATID",
 #'                      countdata = tpm[1:5, ],
 #'                      metadata = metadata,
 #'                      dispersion = disp[1:5],
 #'                      verbose=FALSE)
-#' MS4A1glmm <- glmmQvals(MS4A1glmm, pi0=1)
+#' MS4A1glmm <- glmmQvals(MS4A1glmm)
 
-glmmQvals <- function(glmmResult, cutoff = 0.05, pi0 = NULL, verbose = TRUE) {
+glmmQvals <- function(object, cutoff = 0.05, verbose = TRUE) {
 
-
-  if (!inherits(glmmResult, "GlmmSeq")) {
-    stop("glmmResult must be a GlmmSeq object")
+  if (!(is(object, "GlmmSeq") | is(object, "lmmSeq"))) {
+    stop("object must be a GlmmSeq or lmmSeq object")
   }
 
-  resultStats <- data.frame(glmmResult@stats, check.names = FALSE)
-  for (cn in colnames(resultStats)[grep("P_", colnames(resultStats))]) {
-    q_cn <- gsub("P_", "q_", cn)
-    resultStats[, q_cn] <- NA
-    resultStats[!is.na(resultStats[, cn]), q_cn] <-
-      qvalue(resultStats[!is.na(resultStats[, cn]), cn], pi0 = pi0)$qvalues
-    if (verbose) {
-      cat(paste0("\n", q_cn, "\n"))
-      cat(paste(rep("-", nchar(q_cn)), collapse = ""))
-      print(table(ifelse(resultStats[, q_cn] < cutoff,
+  pvals <- object@stats$pvals
+  qvals <- apply(pvals, 2, function(x) {
+    out <- rep_len(NA, length(x))
+    qv <- try(qvalue(x[!is.na(x)])$qvalues, silent = TRUE)
+    if (class(qv) != 'try-error') {
+      out[!is.na(x)] <- qv
+    } else {
+      out[!is.na(x)] <- p.adjust(x[!is.na(x)], method='BH')
+    }
+    out
+  })
+  rownames(qvals) <- rownames(pvals)
+  
+  if (verbose) {
+    for (i in colnames(qvals)) {
+      cat(paste0("\n", i, "\n"))
+      cat(paste(rep("-", nchar(i)), collapse = ""))
+      print(table(ifelse(qvals[, i] < cutoff,
                          "Significant", "Not Significant")))
     }
   }
-  glmmResult@stats <- as.matrix(resultStats)
-  return(glmmResult)
+  
+  object@stats$qvals <- qvals
+  return(object)
 }
