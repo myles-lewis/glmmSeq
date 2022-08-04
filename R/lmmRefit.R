a99750ad66f928fa4db51f9c76100aa45011d04a
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-08-04 23:08:20 +0100

    Update docs

--- R/lmmRefit.R
+++ R/lmmRefit.R
@@ -1,11 +1,23 @@
 
-
+#' Refit mixed effects model
+#' 
+#' Based on a 'GlmmSeq' or 'lmmSeq' class result object, this function attempts
+#' to refit an identical model for a specific gene based on the data and fitting
+#' parameters stored in the results object and refitting using either
+#' [lme4::glmer()] for `GlmmSeq` objects or `lmer()` for `lmmSeq` objects. The
+#' fitted model can then be passed on to other packages such as `emmeans` to
+#' look at estimated marginal means for the model.
+#' 
+#' @param object A fitted results object of class `GlmmSeq` or `lmmSeq`
+#' @param gene A character value specifying a single gene to extract a fitted
+#'   model for
+#' @return Fitted model of class `lmerMod` in the case of LMM or `glmerMod` for
+#'   a GLMM
 #' @export
 
 lmmRefit <- function(object, gene, ...) {
   UseMethod("lmmRefit")
 }
-
 
 
 #' @export
@@ -24,7 +36,6 @@ lmmRefit.lmmSeq <- function(object, gene, ...) {
   }
   fit
 }
-
 
 
 #' @export
